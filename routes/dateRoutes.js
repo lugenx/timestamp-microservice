1e9df94fa8f3db5ceda319a030516237e9284503
Elgun Asgar <elgunasgerov@gmail.com>
2022-12-08 00:24:22 -0500

    bug fix

--- routes/dateRoutes.js
+++ routes/dateRoutes.js
@@ -4,17 +4,13 @@ const dateRouter = express.Router();
 dateRouter.get("/:date", (req, res) => {
   const query = req.params.date;
 
-  const queryIsDashedDate =
-    /^(\d{4,5}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01]))$/gm.test(query);
-
   const queryIsUnixTime = /^\d+$/.test(query);
+  const date = new Date(query);
 
-  if (queryIsDashedDate) {
-    const date = new Date(query);
+  if (queryIsUnixTime) {
+    res.json({ unix: Number(query), utc: new Date(query * 1).toUTCString() });
+  } else if (date != "Invalid Date") {
     res.json({ unix: date.getTime(), utc: date.toUTCString() });
-  } else if (queryIsUnixTime) {
-    const date = new Date(query * 1).toLocaleDateString();
-    res.json({ unix: Number(query), utc: new Date(date).toUTCString() });
   } else {
     res.json({ error: "Invalid Date" });
   }
